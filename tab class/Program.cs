942d2cf2f520db4d8cc74a16a2c731e3b8c6b58b
Youness <Youness@DESKTOP-A7ECOHN>
2022-03-07 22:14:49 +0100

    Ajoutez des fichiers projet.

---
+++ tab class/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tab_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("donner le nombre de personne: ");
+            int n = int.Parse(Console.ReadLine());
+            string[] tab = new string[n];
+
+           
+
+            for (int i=0; i<n; i++)
+            {
+                tss p1 = new tss();
+                Console.Write("donner le nom: ");
+                tab[i] = p1.nom = Console.ReadLine();
+                Console.Write("donner le prenom: ");
+                tab[i] = p1.prenom = Console.ReadLine();
+                Console.Write("donner le genre: ");
+                tab[i] = p1.genre = Console.ReadLine();
+                Console.Write("donner l'age: ");
+                tab[i] = p1.age = Console.ReadLine();
+
+                p1.affichage();
+                
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.WriteLine("FIN !! " + tab[i]);
+
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ tab class/tss.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tab_class
+{
+    class tss
+    {
+        public string nom;
+        public string prenom;
+        public string age;
+        public string genre;
+
+        public void affichage()
+        {
+            Console.WriteLine("\n\nnom {0}  \nprenom {1}  \nage {2}   \ngenre  {3}\n", nom, prenom, age, genre);
+        }
+    }
+}
